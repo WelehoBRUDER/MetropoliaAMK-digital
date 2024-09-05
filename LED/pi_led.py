88d268ed5b989b2604a6e3f1ae308cdc9d86e011
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-09-05 10:11:20 +0300

    homework

--- LED/pi_led.py
+++ LED/pi_led.py
@@ -1,13 +1,13 @@
 from machine import ADC, Pin
 import time
 led = Pin("LED", Pin.OUT)
-adc = ADC(Pin(27))
-
+adc = ADC(Pin(26))
 
 while True:
     value = adc.read_u16()
     print(value)
     led.on()
-    time.sleep_ms(50)
+    time.sleep_ms(int(value / 131.07)) # 131.07 is 65.535x2
     led.off()
-    time.sleep_ms(50)
+    time.sleep_ms(int(value / 131.07))
+    
