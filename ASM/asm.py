896dabbe2f3154e582f6afd8d92e76356deb89cc
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-10-20 21:04:27 +0300

    work

---
+++ ASM/asm.py
@@ -0,0 +1,90 @@
+from machine import ADC, Pin
+import time
+    
+class Light_asm:
+    def __init__(self, pin_button, pin_lamp, delay):
+        self.button = Pin(pin_button, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.lamp = Pin(pin_lamp, Pin.OUT)
+        self.delay = delay
+        self.state = self.reset
+        
+    def execute(self):
+        print(self.state.__name__)
+        self.state()
+
+    def reset(self):
+        self.lamp.off()
+        time.sleep_ms(self.delay)
+        if not self.button.value():
+            self.state = self.light
+        else:
+            self.state = self.reset
+    
+    def light(self):
+        self.lamp.on()
+        time.sleep_ms(self.delay)
+        if self.button.value():
+            self.state = self.held
+        else:
+            self.state = self.light
+
+            
+    def held(self):
+        self.lamp.on()
+        time.sleep_ms(self.delay)
+        if self.button.value():
+            self.state = self.held
+        else:
+            self.state = self.shut_down
+    
+    def shut_down(self):
+        self.lamp.off()
+        time.sleep_ms(self.delay)
+        if self.button.value():
+            self.state = self.reset
+        else:
+            self.state = self.shut_down 
+
+LIGHT_ASM = Light_asm(7, 20, 50)
+
+def Start_light_asm():
+    while(True):
+        LIGHT_ASM.execute()
+        
+        
+class Alarm_system:
+    def __init__(self, pin_button, pin_alarm, pin_lamp, pin_siren, delay):
+        self.button = Pin(pin_button, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.alarm = Pin(pin_alarm, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.lamp = Pin(pin_lamp, Pin.OUT)
+        self.siren = Pin(pin_siren, Pin.OUT)
+        self.delay = delay
+        self.state = self.reset
+        
+    def execute(self):
+        print(self.state.__name__)
+        self.state()
+    
+    def reset(self):
+        self.lamp.off()
+        self.siren.off()
+        time.sleep_ms(self.delay)
+        self.state = self.check_alarm
+        
+    def check_alarm(self):
+        pass
+        
+    def activate(self):
+        self.lamp.on()
+        self.siren.on()
+        
+ALARM_SYSTEM = Alarm_system(7, 9, 22, 20, 100)
+
+def Start_alarm_system():
+    while(True):
+        ALARM_SYSTEM.execute()
+        
+# Did this to keep both in the same file.
+Start_light_asm()
+# Start_alarm_system()
+
