76ec6880e13d398d3fb82d5b8f1d915d924b06a2
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-10-24 14:25:42 +0300

    rewrite and some comments

--- W-OLED/draw.py
+++ W-OLED/draw.py
@@ -4,26 +4,30 @@ from ssd1306 import SSD1306_I2C
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 screen_width = 128
 screen_height = 64
-center = int((screen_height - 1) / 2)
+center = int((screen_height - 1) / 2) # Rough middle of the screen height
 screen = SSD1306_I2C(screen_width, screen_height, i2c)
 screen.fill(0)
 
 class Draw_controller:
     def __init__(self, up_pin, down_pin, reset_pin):
-        self.x = 0
-        self.y = center
-        self.up = Pin(up_pin, mode=Pin.IN, pull=Pin.PULL_UP)
-        self.down = Pin(down_pin, mode=Pin.IN, pull=Pin.PULL_UP)
-        self.reset = Pin(reset_pin, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.x = 0 # Starting x coordinate
+        self.y = center # Set y to roughly middle of the screen
+        self.up = Pin(up_pin, mode=Pin.IN, pull=Pin.PULL_UP) # SW_2
+        self.down = Pin(down_pin, mode=Pin.IN, pull=Pin.PULL_UP) # SW_0
+        self.reset = Pin(reset_pin, mode=Pin.IN, pull=Pin.PULL_UP) # SW_1
         
+    # Draws pixel at current x and y position.
+    # Also checks if the x position reaches edge of screen.
     def draw_pixel(self):
         if self.x > screen_width:
-            return self.draw_reset()
+            return self.draw_reset(False)
         screen.pixel(self.x, self.y, 1)
         screen.show()
         
-    def draw_reset(self):
-        screen.fill(0)
+    # This function resets the x and y position, and optionally clears the screen.
+    def draw_reset(self, clear = True):
+        if clear:
+            screen.fill(0)
         self.x = 0
         self.y = center
         screen.show()
@@ -32,10 +36,12 @@ class Draw_controller:
         while True:
             self.x += 1
             self.draw_pixel()
-            if self.up.value() == 0 and self.y - 1 >= 0: # Go up
+            if self.up.value() == 0 and self.y - 1 >= 0: # Go up (limited to 0)
                 self.y -= 1
-            elif self.down.value() == 0 and self.y + 1 <= screen_height - 1: # Go down
+            elif self.down.value() == 0 and self.y + 1 <= screen_height - 1: # Go down (limited to max screen height) 
                 self.y += 1
+            elif self.reset.value() == 0: # Clears the screen and resets drawing
+                self.draw_reset()
             
 DRAW = Draw_controller(7, 9, 8)
 DRAW.draw_loop()
--- W-OLED/typing.py
+++ W-OLED/typing.py
@@ -10,35 +10,33 @@ screen.fill(0)
 
 class Text_input:
     def __init__(self):
-        self.history = []
+        self.line = 1 
         
     def start_typing(self):
         while True:
             new_text = input("Type something: ")
-            self.history.append(new_text)
-            self.display_text()
+            self.write_text(new_text)
+            self.line += 1
             
     def scroll_to_latest(self):
-        min_index = 0
-        # How many lines can fit on the screen, assuming the font size is accurate
-        max_display = int(screen.height / font_size)
-        # If the history has more lines than can be displayed, we need to adjust
-        if len(self.history) > max_display:
-            # If history is for example 9, then this will yield 9 - 8 = 1
-            min_index = len(self.history) - max_display
-        return min_index
-            
-            
-    def display_text(self):
-        # Clear the screen
-        screen.fill(0)
-        # Set starting y value to scrolll
-        y = self.scroll_to_latest()
-        # this starts at y, which is at first 0 but then adjusts to fit the latest text in the screen
-        for i in range(y, len(self.history)):
-            txt = self.history[i]
-            screen.text(txt, 0, font_size * (i - y), 1) # Since i = y at first, subtracting y from it gives us a starting height of 0
-            i += 1
+        # Scroll one line down
+        screen.scroll(0, -font_size)
+        # Draw black rectangle over the lowest row on the screen
+        screen.fill_rect(0, screen_height - font_size, screen_width, font_size, 0)
+    
+    # Returns the maximum number of lines the screen can display
+    def max_display(self):
+        return int(screen.height / font_size)
+    
+    def write_text(self, text):
+        # How many lines can be displayed at most
+        limit = self.max_display()
+        # Scroll by one line when at the bottom line
+        if self.line > limit:
+            self.line = limit
+            self.scroll_to_latest()
+        # Print text on screen
+        screen.text(text, 0, (self.line - 1) * font_size)
         screen.show()
 
 TEXT_INPUT = Text_input()
