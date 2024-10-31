0cecded73c52baa017f04158c4e0d5a6d30e932a
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-10-31 14:24:14 +0200

    im bad at code

---
+++ PULSE-DETECTION/positive-peak.py
@@ -0,0 +1,52 @@
+from filefifo import Filefifo 
+data = Filefifo(10, name = 'capture_250Hz_01.txt')
+Fs = 250
+
+peaks = []
+current_slope = 0
+interval_calculator = 0
+
+prev_max = 0
+curr_peak = 0
+
+last_sample = 0
+last_slope = 0
+
+iterator = 0
+
+while len(peaks) < 3:
+    curr_sample = data.get()
+    slope = curr_sample - last_sample
+    if curr_sample > prev_max:
+        prev_max = curr_sample
+        curr_peak = iterator
+    if slope < 0 and last_slope >= 0:
+        peaks.append(curr_peak)
+        prev_max = 0
+        
+    last_sample = curr_sample
+    last_slope = slope
+    iterator += 1
+
+
+        
+        
+times = []
+average_time = 0
+for i in range(1, len(peaks)):
+    times.append((1 / 250) * (peaks[i] - peaks[i - 1]))
+        
+    average_time += times[i - 1]
+    
+average_time = average_time / len(times)
+        
+
+print("Peaks", peaks)
+print("Intervals (s)", times)
+print("Frequency (Hz)", 1 / average_time)
+        
+        
+    
+
+        
+    
