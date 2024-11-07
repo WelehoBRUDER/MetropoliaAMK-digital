d1da568a7ebbf521542b5a6f231a3f8aba76d267
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-11-07 13:32:39 +0200

    added comments

--- ROTARY_ENCODER/led_brightness.py
+++ ROTARY_ENCODER/led_brightness.py
@@ -4,14 +4,18 @@ import time
 
 class Encoder:
     def __init__(self, rot_a, rot_b, rot_c):
+        # A, B relate to the rotation of the encoder
+        # C is the click action
         self.a = Pin(rot_a, mode = Pin.IN, pull = Pin.PULL_UP)
         self.b = Pin(rot_b, mode = Pin.IN, pull = Pin.PULL_UP)
         self.c = Pin(rot_c, mode = Pin.IN, pull = Pin.PULL_UP)
         self.led = PWM(Pin(22, Pin.OUT))
         self.fifo = Fifo(30, typecode = 'i')
         self.prev_click = -200
+        # Set starting and max brightness to brightest possible
         self.brightness = 65535
         self.brightness_limit = 65535
+        # Booleans used because I'm unsure about my coding practices
         self.led_on = False
         self.held = False
         self.a.irq(handler = self.handler, trigger = Pin.IRQ_RISING, hard = True)
@@ -25,11 +29,13 @@ class Encoder:
             self.fifo.put(1)
     
     def click(self):
+        # Prevent holding down the toggle button
         if not self.held:
             self.held = True
             tick = time.ticks_ms()
-            print(tick - self.prev_click >= 200)
+            # Long delay between clicks to prevent jankiness
             if tick - self.prev_click >= 200:
+                # Toggle LED on or off, setting brightness to 0 or max.
                 if not self.led_on:
                     self.led_on = True
                     self.led.duty_u16(self.brightness_limit)
@@ -39,11 +45,15 @@ class Encoder:
                 self.prev_click = tick
             
     def change_brightness(self, value):
+        # Brightness increment is multiplied by 217 because the total value is massive
         self.brightness += value * 217
+        # Check if brightness is outside of limits
+        # and reset it if it is.
         if self.brightness > self.brightness_limit:
             self.brightness = self.brightness_limit
         elif self.brightness < 0:
             self.brightness = 0
+        # Update brightness in the led.
         self.led.duty_u16(self.brightness)
     
 
