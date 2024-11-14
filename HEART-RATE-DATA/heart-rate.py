dac1760dc7093e14df9912b1e2f9e65ab9652f75
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-11-14 13:30:11 +0200

    easiest working wtf

--- HEART-RATE-DATA/heart-rate.py
+++ HEART-RATE-DATA/heart-rate.py
@@ -4,7 +4,7 @@ import time
 from filefifo import Filefifo
 
 # Read the file
-file = Filefifo(10, name = 'capture02_250Hz.txt')
+file = Filefifo(10, name = 'capture03_250Hz.txt')
 # Set how many values should be read from the file
 samples = 2000
 
@@ -13,26 +13,28 @@ current_slope = 0
 interval_calculator = 0
 
 values = []
-for _ in range(samples):
-    val = file.get()
-    values.append(val)
-    
-threshold = int((max(values) + min(values)) / 2)
-
 prev_signal = 0
 prev_max = 0
 prev_slope = 0
 peak = 0
+prev_min = 9999999
+_max = 0
 i = 0
 was_below = True
-while len(peaks) < 20:
+last_batch = []
+threshold = 0
+while len(peaks) < 40:
     signal = file.get()
     slope = signal - prev_signal
+    if signal < prev_min:
+        prev_min = signal
+    #print("signal:",signal, "threshold:",threshold)
     if signal > threshold and signal > prev_max:
         peak = i
         prev_max = signal
     if signal > threshold and slope <= 0 and prev_slope > 0:
         if was_below:
+            threshold = int((prev_max + prev_min) / 2)
             peaks.append(peak)
             prev_max = 0
             was_below = False
@@ -49,4 +51,17 @@ average_time = 0
 for i in range(len(peaks) - 1):
     time = ((1 / 250) * (peaks[i + 1] - peaks[i]))
     hr.append(int(60 / time))
-print(hr)
+    
+heart_rates = []
+         
+for i in range(len(hr)):
+    time = hr[i]
+    if i > 0:
+        if not time > hr[i - 1] * 1.5 and not time < hr[i - 1] * 0.6 and not time > 240 and not time < 40:
+             heart_rates.append(time)
+    else:
+         heart_rates.append(time)
+        
+    
+   
+print(heart_rates)
