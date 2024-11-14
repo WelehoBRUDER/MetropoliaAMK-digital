0226147b5c7b8ab4ecf24e3776e3349a472b4a33
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-11-14 14:01:21 +0200

    changed to use classes

--- HEART-RATE-DATA/heart-rate.py
+++ HEART-RATE-DATA/heart-rate.py
@@ -5,63 +5,73 @@ from filefifo import Filefifo
 
 # Read the file
 file = Filefifo(10, name = 'capture03_250Hz.txt')
-# Set how many values should be read from the file
-samples = 2000
 
-peaks = []
-current_slope = 0
-interval_calculator = 0
+class HeartMaster:
+    def __init__(self):
+        self.min = 99999999
+        self.max = 0
+        self.peak = 0
+        self.signal = 0
+        self.thresh = 0
+        self.peaks = []
+        self.count = 1
+        self.dipped = True
+        
+    def set_thresh(self):
+        if self.signal > self.max:
+            self.max = self.signal
+        if self.signal < self.min:
+            self.min = self.signal
+        
+    def calc_thresh(self):
+        self.thresh = int((self.max + self.min) / 2)
+        self.max = 0
+        
+    def measure(self):
+        if(self.count % 250 == 0):
+            self.calc_thresh()
+            self.prev_max = 0
+            
+        self.set_thresh()
+            
+        #print("signal:",signal, "threshold:",threshold)
+        if self.signal > self.thresh:
+            self.peak = self.count
+            
+        if self.signal > self.thresh:
+            if self.dipped:
+                self.peaks.append(self.peak)
+                self.dipped = False
+        if self.signal <= self.thresh:
+            self.dipped = True
+        
+        self.count += 1
+        
+    def get_heart_rates(self):
+        hr = []
+        average_time = 0
+        for i in range(len(self.peaks) - 1):
+            time = ((1 / 250) * (self.peaks[i + 1] - self.peaks[i]))
+            hr.append(int(60 / time))
+            
+        heart_rates = []
+                 
+        for i in range(len(hr)):
+            time = hr[i]
+            if i > 0:
+                if not time > hr[i - 1] * 1.5 and not time < hr[i - 1] * 0.5 and not time > 240 and not time < 40:
+                     heart_rates.append(time)
+            else:
+                 heart_rates.append(time)
+                 
+        return heart_rates
+        
+heart_master = HeartMaster()
 
-values = []
-prev_signal = 0
-prev_max = 0
-prev_slope = 0
-peak = 0
-prev_min = 9999999
-_max = 0
-i = 0
-was_below = True
-last_batch = []
-threshold = 0
-while len(peaks) < 40:
-    signal = file.get()
-    slope = signal - prev_signal
-    if signal < prev_min:
-        prev_min = signal
-    #print("signal:",signal, "threshold:",threshold)
-    if signal > threshold and signal > prev_max:
-        peak = i
-        prev_max = signal
-    if signal > threshold and slope <= 0 and prev_slope > 0:
-        if was_below:
-            threshold = int((prev_max + prev_min) / 2)
-            peaks.append(peak)
-            prev_max = 0
-            was_below = False
-    if signal <= threshold:
-        prev_max = 0
-        was_below = True
-    prev_signal = signal
-    prev_slope = slope
-    i += 1
-        
-        
-hr = []
-average_time = 0
-for i in range(len(peaks) - 1):
-    time = ((1 / 250) * (peaks[i + 1] - peaks[i]))
-    hr.append(int(60 / time))
+while len(heart_master.peaks) < 40:
+    heart_master.signal = file.get()
+    heart_master.measure()
     
-heart_rates = []
-         
-for i in range(len(hr)):
-    time = hr[i]
-    if i > 0:
-        if not time > hr[i - 1] * 1.5 and not time < hr[i - 1] * 0.6 and not time > 240 and not time < 40:
-             heart_rates.append(time)
-    else:
-         heart_rates.append(time)
         
-    
-   
-print(heart_rates)
+heart_rate_values = heart_master.get_heart_rates()
+print(heart_rate_values)
